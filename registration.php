6e8b24cd88b84e96c4be7c781b1e30d0e867937e
Mohamed Ngarama <>
2021-04-17 12:53:59 +0000

    Made it responsive

--- registration.php
+++ registration.php
@@ -13,11 +13,22 @@
 
 <body>
   <h1>Welcome at Mohamed's First Page</h1>
+
   <h3>Please register or login for More!</h3>
 
+  <p>
+  <?php
+    if(isset($_SESSION['error']) && !empty($_SESSION['error'])){
+      echo $_SESSION['error'];
+
+      session_unset();
+    }
+  ?>
+  </p>
+
   <form action="info.php" method="POST">
-    <input type="text" name="email" placeholder="Email" required><br>
-    <input type="password" name="password" placeholder="password" required><br>
+    <input type="text" name="email" placeholder="Email" ><br>
+    <input type="password" name="password" placeholder="password"><br>
 
     <button style="background-color:#dbf6e9;" type="submit" name="register">Register</button>
 
